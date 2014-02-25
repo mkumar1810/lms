220f04a63c94741b6441b9cff712f94e681d41a7
Mohan Kumar <m_kumar1810@yahoo.com>
2014-02-25 20:21:42 +0530

    third commit

---
+++ sublimelms/Utilities/defaults.h
@@ -0,0 +1,13 @@
+//
+//  defaults.h
+//  sublimelms
+//
+//  Created by Macintosh User on 25/2/14.
+//  Copyright (c) 2014 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface defaults : NSObject
+
+@end
